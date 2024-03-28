6e010e74cb751437606a03d3ddd82b927744e75c
한승훈 <seunghoon526@naver.com>
2024-03-28 22:44:52 +0900

    Initial commit

---
+++ movie-app/src/components/Movie.js
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from 'react'
+
+function Movie(){
+
+    const [movieList, setMovieList] = useState([])
+    const getMovie = () =>{
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=61ff0cec6a2b6307cab2c3c7b1957809")
+            .then(res => res.json())
+            .then(json => setMovieList(json.results))
+    }
+
+    useEffect(() => {
+        getMovie()
+    }, []);
+
+    console.log(movieList)
+
+    return (
+        <div>
+            {movieList.map(()=>(
+                <img style={{width:"300px", height:"250px", marginLeft: "10px", marginTop:"10px"}} src ={`https://image.tmdb.org/t/p/w500${Movie.poster_path}`}/>
+            ))}
+        </div>
+    )
+}
+
+export default Movie
