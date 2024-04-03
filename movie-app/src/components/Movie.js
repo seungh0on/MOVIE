2d73f88bc786ebafc3a4ebf4502a66cdb8b888e6
한승훈 <seunghoon526@naver.com>
2024-04-03 23:16:55 +0900

    fix: index.js and Movie.js

--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -1,25 +1,30 @@
-// Movie.js
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Movie(){
 
-    const [movieList, setMovieList] = useState([])
+    const [movieList, setMovieList] = useState([]);
+
     const getMovie = () =>{
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=61ff0cec6a2b6307cab2c3c7b1957809")
-            .then(res => res.json())
-            .then(json => setMovieList(json.results))
+        axios.get("https://api.themoviedb.org/3/discover/movie?api_key=61ff0cec6a2b6307cab2c3c7b1957809")
+            .then(response => {
+                setMovieList(response.data.results);
+            })
+            .catch(error => {
+                console.error('There was a problem fetching the movies:', error);
+            });
     }
 
     useEffect(() => {
-        getMovie()
+        getMovie();
     }, []);
 
-    console.log(movieList)
+    console.log(movieList);
 
     return (
         <div>
-            {movieList.map((movie)=>(
-                <img style={{width:"300px", height:"250px", marginLeft: "10px", marginTop:"10px"}} src ={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+            {movieList.map((movie) => (
+                <img style={{width:"300px", height:"250px", marginLeft: "10px", marginTop:"10px"}} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
             ))}
         </div>
     )
